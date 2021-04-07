25a5ffc82183e63b13c6493b770b5976b6853293
cocostru <dk.pochtamp@gmail.com>
2021-04-08 02:13:19 +0800

    verbfilter post

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'dam@icrypto.world',
     'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
+    'senderEmail' => 'dam-form@icrypto.world',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'dam@icrypto.world',
 ];
