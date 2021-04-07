f1fc9326c20bea77d12598ec328555ee34c86ab6
cocostru <dk.pochtamp@gmail.com>
2021-04-07 19:07:35 +0800

    verbfilter post

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         // 'css/site.css',
+        'css/font.css',
         'css/font-awesome.css',
         'css/owl.carousel.css',
         'css/owl.theme.default.css',
