ce46db03c093bc13905ac5d4a5ea821d3669565a
cocostru <dk.pochtamp@gmail.com>
2021-08-30 04:08:07 +0400

    first trial

--- backend/views/mail/index.php
+++ backend/views/mail/index.php
@@ -204,6 +204,8 @@ textarea {
                 <span class="label label-dep label-grey" data-dep="contact@damfz.co">contact</span>
                 <span class="label label-dep label-grey" data-dep="support@damfz.co">support</span>
                 <span class="label label-dep label-grey" data-dep="promo@damfz.co">promo</span>
+                <span class="label label-dep label-grey" data-dep="nurgiz@damfz.co">nurgiz</span>
+                <span class="label label-dep label-grey" data-dep="eduard@damfz.co">eduard</span>
             </div><br/><br/><br/>
 
 
@@ -322,94 +324,19 @@ textarea {
 
         <br/><br/>
 
-        <div class="mail-sample mail-sample-marketing hidden" style="font-size:13px; color: #aab; padding: 15px 20% 15px 5px;white-space: pre-wrap">
-Re: Здравствуйте подскажите раздел рекламные материалы планируется?
+        <div class="mail-sample mail-sample-tfa" style="font-size:13px; color: #aab; padding: 15px 20% 15px 5px;white-space: pre-wrap"> Re: (subject of conversation)
 ...
 
-Здравствуйте, 
-Спасибо, что связались с нами, мы всегда рады помочь!
+Dear John,
 
-Если ваш вопрос касается {smth ...} , то ответ - да!) 
-Мы рады, что Вы задали этот вопрос, и хотели бы {smth ...}
+Glad to hear to hear you,
 
-Также, если не вызовет затруднений, могли бы Вы ответить на два вопроса?
-
-1. Где бы Вы хотели разместить рекламные материалы?
-а) Доступный по сети (www) ресурс (пожалуйста, уточните характер ресурса: форум, новостной ресурс, агрегатор контента, соц. сети, блог/видеоблог и т.д.)
-б) Мобильное приложение (уточните адрес приложения) 
-в) Медиа источники (не www) (уточните сферу: газета, ТВ, радио и т.д.)
-г) Мероприятие/коференция (пожалуйста, уточните название мероприятия и дату)
-
-2. Могли бы Вы немного рассказать о вашей специализации и сфере занятости? Чтобы у нас появилась возможность провести анализ и сформировать максимально эффективное взаимодействие.
-
-Мы надеемся, что сможем Вам помочь в любом случае, поэтому хотели бы, чтобы наш диалог продолжился, а Вы ответили на это письмо. Возможно, получится что-то решить в рамках вашего вопроса индивидуально, если {smth ...}
-
-С уважением,
-команда iCrypto.world
-        </div>
-
-        <div class="mail-sample mail-sample-tfa" style="font-size:13px; color: #aab; padding: 15px 20% 15px 5px;white-space: pre-wrap">
-Re: Показывает что 2FA-код не верный
 ...
 
-Здравствуйте, 
-Спасибо, что связались с нами, мы всегда рады помочь! 
-
-Пожалуйста, сообщите адрес страницы и опишите при каких событиях происходит отказ по 2FA-коду.
-
-Мы надеемся, что сможем Вам помочь в любом случае, поэтому хотели бы, чтобы наш диалог продолжился, а Вы ответили на это письмо.
-
-С уважением,
-команда iCrypto.world
+Cordially, Eduard
         </div>
 
-        <div class="mail-sample mail-sample-tfa-signin hidden" style="font-size:13px; color: #aab; padding: 15px 20% 15px 5px;white-space: pre-wrap">
-Re: please reset my 2FA need to withdraw fund and its not working
-...
 
-Hello, {name},
-We're glad you've contacted us & ready to help!
-
-The subject of the ticket defines your 2FA-key is stale or lost and you cannot sign into the system, - <u>could you approve that is right</u>?
-
-<u>If so</u>, you will be required to provide us your current phone number, to which we shall send one-time-password message (sms).
-
-<u>Else</u> (sign-in-action 2FA-requirement is off), below is applicable:
-
-To reset your 2FA-key, we require to re-verify your identity, to do so we will remove your phone-number record, so you could add & verify your current one (by sms one-time-password).
-
-<u>Could you, also</u>, help us a bit, and tell within what conditions your 2FA-code is denied?
-Also, please, recall a precise URL of the action-page.
-
-Sometimes, our clients are a bit confused about our graphical user interface or experience, - so we try to pull out any helpful information on any case or issue of the clients.
-
-Cordially,
-iCrypto.world team.
-        </div>
-
-        <div class="mail-sample mail-sample-accrual hidden" style="font-size:13px; color: #aab; padding: 15px 20% 15px 5px;white-space: pre-wrap">
-Re: Здравствуйте купила два дня назад мощность а начислений так и не. можнте помочь разобраться?
-...
-
-Здравствуйте, 
-Спасибо, что связались с нами, мы всегда рады помочь!
-
-Если ваш вопрос касается кластера для облачного клиентского майнинга, - приносим извинения за длительные задержки, настройка оборудования может занимать до 48-и часов и еще 24+ могут уйти на то чтобы оборудование наработало первый вклад в нахождение блока, с которого будет получена награда начисляемая Вам.
-
-Мы просим Вас зайти в личный кабинет и проверить начисления еще раз.
-
-Также, если не вызовет затруднений, могли бы Вы ответить на два вопроса?
-
-1. Могли бы Вы немного рассказать, каким образом Вы получили код-приглашение в данный проект? Чтобы мы смогли проанализировать рациональность нашей продуктовой политики и появилась дополнительная возможность сформировать максимально эффективное взаимодействие с нашими клиентами.
-
-2. Могли бы Вы немного рассказать о вашей специализации и сфере занятости?
-
-Мы надеемся, что сможем Вам помочь в любом случае, поэтому хотели бы, чтобы наш диалог продолжился, а Вы ответили на это письмо. Мы абсолютно уверены, что это невозможно, но если задержка с даты приобритения мощностей до первого начисления составляет более 84 часов, то, пожалуйста, сообщите нам об этом, - мы обязательно постараемся все решить.
-
-С уважением,
-команда iCrypto.world
-        
-        </div>
     </div>
 
 </div>
