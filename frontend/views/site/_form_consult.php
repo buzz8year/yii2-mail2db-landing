6d688fa1e60eff84926ecf9c1e526a6a77742d63
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 01:06:11 +0400

    Update _form_consult.php

--- frontend/views/site/_form_consult.php
+++ frontend/views/site/_form_consult.php
@@ -2,8 +2,8 @@
     <div class="feedback_form">
         <div class="feedback_form_lbl">
         <div class="feedback_form_lbl_wr">
-            <h2><?= Yii::t('app', 'Консультация') ?></h2>
-            <div class="feedback_form_txt"><?= Yii::t('app', 'Пожалуйста, заполните данные. Мы с вами свяжемся!') ?></div>
+            <h2>Get Professional Consultation</h2>
+            <div class="feedback_form_txt">Please, leave us your contacts, so we could get in touch with you.</div>
         </div>
         </div>
         <div class="feedback_form_wr">
@@ -11,33 +11,25 @@
             <div class="start1">
             
                 <div class="form-group">
-                    <label for="exampleInputEmail1"><?= Yii::t('app', 'Имя') ?></label>
-                    <input type="text" class="form-control required" name="consult-name"  placeholder="Имя">
+                    <label for="exampleInputEmail1">Your Name</label>
+                    <input type="text" class="form-control required" name="partner-name"  placeholder="Name">
                 </div>
-
                 <div class="form-group">
-                    <label for="exampleInputPassword1"><?= Yii::t('app', 'Телефон') ?></label>
-                    <input type="text" class="form-control required poletelephon" name="consult-phone"  placeholder="Телефон">
-                </div>
+                    <label for="exampleInputPassword1">Your Phone</label>
+                    <input type="text" class="form-control required poletelephon" name="partner-phone"  placeholder="Phone">
+                </div>    
 
-                <div class="form-group">
-                    <label for="exampleInputPassword1"><?= Yii::t('app', 'Вопрос') ?></label>
-                    <textarea class="form-control required" name="consult-body"  placeholder="Вопрос"></textarea>
-                </div> 
-
-                <div class="form-group-desc"><?= Yii::t('app', '* нажимая кнопку «Отправить», я даю свое согласие на обработку моих персональных данных, в соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных»') ?></div>
-
+                <div class="form-group-desc">By clicking the “Submit” button, I give my consent to the processing of my personal data, in accordance with Federal Law No.A123 “On Personal Data”, under the conditions and for the purposes specified in the Consent to the processing of personal data *</div>
                 <div style="height:15px;"></div>
-
-                <button type="submit" class="btn btn-primary post-consult"><?= Yii::t('app', 'Отправить') ?></button>   
+                <button type="submit" class="btn btn-primary post-partner">Submit</button>   
             
             </div>
                         
             <div class="end1">
-                <div><?= Yii::t('app', 'Спасибо, ваше сообщение принято.') ?></div>
+                <div>Thank you! We got your message.</div>
             </div>
             <div class="msg-error hidden">
-                <div>Ошибка.</div>
+                <div>Error.</div>
             </div>
         </div>
     </div>
