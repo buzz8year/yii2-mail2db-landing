26822c316b116560513677435f91578cfb7667b6
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 00:42:35 +0400

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -67,8 +67,8 @@
         <div class="row">
             <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
             
-                <div class="block1-label1">Digital Asset Management</div>
-                <div class="block1-label2"><?= Yii::t('app', 'Промышленный хостинг компьютерного оборудования и разработка цифровых решений для вашего бизнеса') ?></div>
+                <div class="block1-label1">Your Company Name</div>
+                <div class="block1-label2">Some text to wrap head around what your business is and why you effective.</div>
                 <div class="block1-btn">
                     <!-- <button class="btn1 btn btn-primary1 btn-right popupbutton1" href="#popupform_text1">Подробнее</button> -->
                     <button class="btn2 btn btn-primary2 popupbutton1" href="#popupform1"><?= Yii::t('app', 'Получить консультацию') ?></button>
@@ -86,7 +86,7 @@
             <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
                 
                 <div class="block2-label1"><?= Yii::t('app', 'О проекте') ?></div>
-                <div class="block2-label2"><?= Yii::t('app', 'Необходимость внедрения IT решений в бизнесе стремительно набирают популярность в качестве эффективных инструментов и средств менеджмента, постепенно, но уверенно становясь значимой частью каждого предприятия. Увеличение экономического влияния IT индустрии на международных рынках сопровождается уверенным ростом их капитализации, с каждым годом ставящей новые рекорды.') ?></div>
+                <div class="block2-label2">Some text to wrap head around what your business is and why you effective.</div>
                 
             </div>
             <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
@@ -126,13 +126,13 @@
         
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">           
-                <div class="block5-label1"><?= Yii::t('app', 'BIG DATA — НЕФТЬ НАШЕГО ВРЕМЕНИ') ?></div>         
+                <div class="block5-label1">Some text to wrap head around what your business is and why you effective.</div>         
             </div>
         </div>
         
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">           
-                <div class="block5-label2"><?= Yii::t('app', 'ЦОД может быть предназначен как для использования одним предприятием, так и являться  многопользовательским.  Многопользовательский ЦОД предоставляет широкий спектр услуг, среди которых business continuity & disaster recovery (непрерывность бизнеса), хостинг, аренда сервера, размещение сервера и т.д. Обращение к услугам многопользовательского ЦОД наиболее целесообразно для компаний среднего и малого бизнеса, т.к. позволяет избежать капитальных затрат на модернизацию ИТ-инфрастурктуры и получить высококачественный сервис и гарантию надежности.') ?></div>           
+                <div class="block5-label2">Some text to wrap head around what your business is and why you effective.</div>           
             </div>
         </div>
         <div style="height:90px;"></div>
@@ -143,7 +143,7 @@
                 </a>            
             </div>      
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">         
-                <div class="block5-label3"><?= Yii::t('app', 'Где YouTube хранит видео, а Instagram – фотографии, сколько это весит и во сколько обходится компаниям? Кто задумывается о том, где конкретно хранятся и обрабатываются эти данные? Сейчас в мире около 50 млрд подключенных к интернету устройств. Каждый житель Земли генерирует 1,7 Мб информации в секунду и эти показатели растут с каждым днем. 30% всех данных хранится и обрабатывается в облаке. Сегодня развитие технологий идет рука об руку с ростом объема данных. И чем дальше, тем больше их будет. Речь идет о востребованных данных о поведении пользователей, Big Data, искусственном интеллекте.') ?></div>          
+                <div class="block5-label3">Some text to wrap head around what your business is and why you effective.</div>          
             </div>
         </div>  
     </div>
@@ -153,10 +153,10 @@
     <div class="container-fluid container-fluid1">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
-                <div class="block6-label1"><?= Yii::t('app', 'СЕЙЧАС ЛУЧШЕЕ ВРЕМЯ ДЛЯ РОСТА ДАТА-ЦЕНТРОВ') ?></div> 
-                <div class="block6-label2"><?= Yii::t('app', 'И вычислительных мощностей') ?></div> 
-                <div class="block6-label3"><?= Yii::t('app', 'Положитесь на экспертов') ?></div>    
-                <div class="block6-label4"><?= Yii::t('app', 'В 2021 году дата-центр стремительно наращивает парк вычислительных мощностей, соответствуя всё возрастающему спросу на дата центры. Открыты возможности совместных вложений в оборудование. Специалисты центра помогут сделать выбор и возьмут все заботы на себя.') ?></div>
+                <div class="block6-label1">Some text to wrap head around what your business is and why you effective.</div> 
+                <div class="block6-label2">Some text to wrap head around what your business is and why you effective.</div> 
+                <div class="block6-label3">Some text to wrap head around what your business is and why you effective.</div>    
+                <div class="block6-label4">Some text to wrap head around what your business is and why you effective.</div>
                 <div class="block6-btn" style="margin-bottom: 60px">
                     <button class="btn4 btn btn-primary2 popupbutton1" href="#popupform1"><?= Yii::t('app', 'Получить консультацию') ?></button>
                 </div>              
@@ -173,8 +173,8 @@
                                 <img src="images/server.svg">
                             </div>
 
-                            <!-- <div class="box-tovar-label1 hidden">ANTMINER S17 73TH</div>
-                            <div class="box-tovar-label2 hidden">Еще пару лет назад устройства, способные разогнаться до 53 TH/s, считались самыми продуктивными. Однако недавно ведущий производитель представил модель Bitmain Antminer s17 73th.<br/>Сообщение о запуске продаж новых ASIC взволновало целые корпорации, готовые купить тысячи устройств. Все дело в низком энергопотреблении и в выдаваемом хешрейта. Новый АСИК презентовали в конце 2019 года в Германии во время мероприятия World Digital Mining Summit.</div> -->
+                            <!-- <div class="box-tovar-label1 hidden">Product A</div>
+                            <div class="box-tovar-label2 hidden">Some text to wrap head around what your business is and why you effective.</div> -->
                         
                         </div>
                     </div>
@@ -194,8 +194,8 @@
             <div class="logo1"><img src="images/logo1.png"></div>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <div class="block7-label1">Digital Asset Management</div>
-            <div class="block7-label2">— <?= Yii::t('app', 'один из крупнейших дата-центров в Восточной Сибири с мощностью свыше 5 МВт. Мы занимаемся промышленным хостингом компьютерного оборудования и разработкой IT решений для управления компьютерными сетями.') ?></div>
+            <div class="block7-label1">Your Company Name</div>
+            <div class="block7-label2">Some text to wrap head around what your business is and why you effective.</div>
             <div style="clear:both;"></div>
         </div>      
     </div>
@@ -206,15 +206,15 @@
     <div class="container-fluid container-fluid1">
     <div class="row">
         <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-            <div class="block8-label1"><?= Yii::t('app', 'Мы предлагаем') ?></div>
-            <div class="block8-label2">— <?= Yii::t('app', 'Хостинг компьютерного оборудования в дата-центрах') ?></div>
+            <div class="block8-label1">Service A</div>
+            <div class="block8-label2">Some text to wrap head around what your business is and why you effective.</div>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
             
             <div class="box-part1">
                 <div class="box-circle1 box-circle">
                     <div class="box-circle1-wr">
-                        <div class="box-circle1-label1"><?= Yii::t('app', 'Разработка программного обеспечения и IT решений для Вашего бизнеса') ?></div>                   
+                        <div class="box-circle1-label1">Some text to wrap head around what your business is and why you effective.</div>                   
                     </div>
                 </div>
                 <div style="clear:both;"></div>             
@@ -223,7 +223,7 @@
             <div class="box-part2">
                 <div class="box-circle2 box-circle">
                     <div class="box-circle2-wr">
-                        <div class="box-circle2-label1"><?= Yii::t('app', 'Услуги облачных решений и аренды вычислительных мощностей для вашего бизнеса') ?></div>                  
+                        <div class="box-circle2-label1">Some text to wrap head around what your business is and why you effective.</div>                  
                     </div>
                 </div>
                 <div style="clear:both;"></div>                 
@@ -243,7 +243,7 @@
             <div class="box-part1">
                 <div class="box-circle3 box-circle">
                     <div class="box-circle3-wr">
-                        <div class="box-circle3-label1"><?= Yii::t('app', 'Поставка и хостинг компьютерного оборудования в дата-центрах') ?></div>                   
+                        <div class="box-circle3-label1">Service B</div>                   
                     </div>
                 </div>
                 <div style="clear:both;"></div>             
@@ -252,7 +252,7 @@
             <div class="box-part2">
                 <div class="box-circle4 box-circle">
                     <div class="box-circle4-wr">
-                        <div class="box-circle4-label1"><?= Yii::t('app', 'Консалтинговые услуги в сфере информационных технологий') ?></div>                   
+                        <div class="box-circle4-label1">Service C</div>                   
                     </div>
                 </div>
                 <div style="clear:both;"></div>                 
@@ -270,8 +270,8 @@
     <div class="container-fluid container-fluid1">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="block9-label1"><?= Yii::t('app', 'ГАРАНТИИ') ?></div>
-            <div class="block9-label2"><?= Yii::t('app', 'Подтверждение единицей оборудования 1:1') ?></div>
+            <div class="block9-label1">Guarantee</div>
+            <div class="block9-label2">Something and some othe stuff</div>
         </div>
     </div>
     <div class="row">
@@ -279,42 +279,42 @@
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">      
             <div class="box-plus">
                 <div class="box-plus-img1 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Фотофиксация всех этапов покупки и установки') ?></div>
+                <div class="box-plus-label">Advantage B</div>
             </div>      
         </div>
             
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">  
             <div class="box-plus">
                 <div class="box-plus-img2 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Индивидуальный серийный номер, подпись полки именем владельца') ?></div>
+                <div class="box-plus-label">Advantage H</div>
             </div>      
         </div>      
         
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">  
             <div class="box-plus">
                 <div class="box-plus-img3 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Видеонаблюдение, трансляция на смартфон владельца') ?></div>
+                <div class="box-plus-label">Advantage Q</div>
             </div>      
         </div>
         
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">      
             <div class="box-plus">
                 <div class="box-plus-img4 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Всё оформляется в соответствии с законодательством РФ') ?></div>
+                <div class="box-plus-label">Advantage P</div>
             </div>      
         </div>
             
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">  
             <div class="box-plus">
                 <div class="box-plus-img5 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Мгновенный доступ к онлайн отчётности. Онлайн мониторинг') ?></div>
+                <div class="box-plus-label">Advantage W</div>
             </div>      
         </div>      
         
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">  
             <div class="box-plus">
                 <div class="box-plus-img6 box-plus-img"></div>
-                <div class="box-plus-label"><?= Yii::t('app', 'Реальная или виртуальная экскурсия по первому требованию') ?></div>
+                <div class="box-plus-label">Advantage Y</div>
             </div>      
         </div>
     
@@ -334,21 +334,21 @@
                         
                         <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12">
                         
-                            <div class="box-forma-label1"><?= Yii::t('app', 'ОСТАЛИСЬ ВОПРОСЫ?') ?></div>    
-                            <div class="box-forma-label2"><?= Yii::t('app', 'Оставьте ваши контактные данные и мы перезвоним вам в ближайшее время') ?></div>   
+                            <div class="box-forma-label1">Have a question?</div>    
+                            <div class="box-forma-label2">Please, leave us your message and contacts, so we could get back to you.</div>   
                                 
                             <div class="box-forma1">
                                 <div class="box-start1 start1">    
-                                    <div class="inp1 "><input type="text" class="inppole required" value=""  name="consult-name" placeholder="<?= Yii::t('app', 'Имя') ?>"/></div>
-                                    <div class="inp1 "><input type="text" class="inppole required" value=""  name="consult-phone" placeholder="<?= Yii::t('app', 'Телефон или Email') ?>"/></div>
-                                    <div class="inp2 "><textarea name="consult-body" class="inppole required" placeholder="<?= Yii::t('app', 'Заявка или запрос на консультацию') ?>"></textarea></div>
+                                    <div class="inp1 "><input type="text" class="inppole required" value=""  name="consult-name" placeholder="Name"/></div>
+                                    <div class="inp1 "><input type="text" class="inppole required" value=""  name="consult-phone" placeholder="Email"/></div>
+                                    <div class="inp2 "><textarea name="consult-body" class="inppole required" placeholder="Your message..."></textarea></div>
                                     <div class="inp3"><?= Yii::t('app', 'Нажимая кнопку «Отправить», я даю свое согласие на обработку моих персональных данных, в соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных», на условиях и для целей, определенных в Согласии на обработку персональных данных *') ?></div>
                                     
-                                    <button class="btn btn-primary5 post-consult"><?= Yii::t('app', 'Отправить') ?></button>
+                                    <button class="btn btn-primary5 post-consult">Send</button>
                                     
                                 </div>
                                 <div class="box-end1 end1">
-                                    <div><?= Yii::t('app', 'Спасибо, ваше сообщение принято.') ?></div>
+                                    <div>Thank you! We got your message.</div>
                                 </div>
                             </div>
                         </div>
@@ -385,8 +385,8 @@
                 <div class="box-contacts">
                     <img class="box-plus-img" src="images/c2.png">
                     <div class="box-contacts1">
-                        <div class="box-contacts-label1"><?= Yii::t('app', 'Почта') ?>:</div>
-                        <div class="box-contacts-label2" style="font-size: 23px">damfzco@gmail.com</div>                    
+                        <div class="box-contacts-label1">Email:</div>
+                        <div class="box-contacts-label2" style="font-size: 23px">yourcompany@gmail.com</div>                    
                     </div>
                     <div style="clear:both;"></div>
                 </div>
@@ -402,8 +402,8 @@
                 <div class="box-contacts box-contacts-end">
                     <img class="box-plus-img" src="images/c3.png">
                     <div class="box-contacts1">
-                        <div class="box-contacts-label1"><?= Yii::t('app', 'Телефон') ?>:</div>
-                        <div class="box-contacts-label2" style="font-size: 23px">+971 585 258 333</div>                  
+                        <div class="box-contacts-label1">Phone:</div>
+                        <div class="box-contacts-label2" style="font-size: 23px">+999 555 555 555</div>                  
                     </div>
                     <div style="clear:both;"></div>
                 </div>
@@ -416,8 +416,8 @@
                 <div class="box-contacts">
                     <img class="box-plus-img" src="images/c1.png">
                     <div class="box-contacts1">
-                        <div class="box-contacts-label1"><?= Yii::t('app', 'Адрес') ?>:</div>
-                        <div class="box-contacts-label2" style="font-size: 23px">Build. A2, Dubai Digital Park, Dubai Silicon Oasis, Dubai, United Arab Emirates</div>                   
+                        <div class="box-contacts-label1">Legal Address:</div>
+                        <div class="box-contacts-label2" style="font-size: 23px">Build. A2, Smith Avenue, New York, USA</div>                   
                     </div>
                     <div style="clear:both;"></div>
                 </div>
