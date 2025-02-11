f71269bde1744b81b235700a593549f75b8aabf0
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 00:58:35 +0400

    Update _form_text.php

--- frontend/views/site/_form_text.php
+++ frontend/views/site/_form_text.php
@@ -1,9 +1,5 @@
 <div id="popupform_text1" class="popupform popupform2 frm1">
     <div class="feedback_form">
-        <div class="feedback_form_wr">
-                    
-            Текст   
-            
-        </div>
+        <div class="feedback_form_wr"></div>
     </div>
 </div>
